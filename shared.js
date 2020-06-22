33a52c1e0727f1a9fe6aed0717ad207d8143f6dd
Michael Newman <coloradohokie@gmail.com>
2020-06-21 23:06:01 -0600

    add animations. End of Section 16

--- shared.js
+++ shared.js
@@ -3,22 +3,26 @@ const selectPlanButtons = document.querySelectorAll('.plan button')
 const modal = document.querySelector('.modal')
 const toggleButton = document.querySelector('.toggle-button')
 const mobileNav = document.querySelector('.mobile-nav')
+const ctaButton = document.querySelector(".main-nav__item--cta")
 
 function closeModal() {
-    // modal.style.display = 'none'
-    // backdrop.style.display = 'none'
     if (modal) {
         modal.classList.remove('open')
     }
-    backdrop.classList.remove('open')
+    setTimeout(function() {
+        backdrop.classList.remove('open')
+    }, 200)
+    backdrop.style.display = 'none'
+
 }
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
     selectPlanButtons[i].addEventListener('click', function() {
-        // modal.style.display = 'block';
-        // backdrop.style.display = 'block'
         modal.classList.add('open')
-        backdrop.classList.add('open')
+        backdrop.style.display = 'block'
+        setTimeout(function() {
+            backdrop.classList.add('open')
+        }, 10)                
     })
 }
 
@@ -28,7 +32,6 @@ for (let i=0; i< selectModalButtons.length; i++) {
 }
 
 backdrop.addEventListener('click', function() {
-    // mobileNav.style.display = 'none'
     mobileNav.classList.remove('open')
     closeModal()
 
@@ -36,8 +39,21 @@ backdrop.addEventListener('click', function() {
 
 
 toggleButton.addEventListener('click', function() {
-    // mobileNav.style.display = 'block'
-    // backdrop.style.display = 'block'
     mobileNav.classList.add('open')
-    backdrop.classList.add('open')
+    backdrop.style.display = 'block'
+    setTimeout(function() {
+        backdrop.classList.add('open')
+    }, 10)
+})
+
+ctaButton.addEventListener('animationstart', function() {
+    console.log('Animation started', event)
+})
+
+ctaButton.addEventListener('animationend', function() {
+    console.log('Animation ended', event)
+})
+
+ctaButton.addEventListener('animationiteration', function() {
+    console.log('Animation iteration', event)
 })
